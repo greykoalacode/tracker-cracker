5d6c17c6eeecbb7db8f333c335447b2d968bf821
greykoalacode <kmaddy9122@gmail.com>
2022-05-23 23:43:02 +0530

    js-cookie added

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -1,12 +1,17 @@
-export function getCookie(cookieName) {
-    var cookieArr = document.cookie.split(";");
-    for (var i = 0; i < cookieArr.length; i++) {
-      var cookiePair = cookieArr[i].split("=");
-      if (cookieName === cookiePair[0].trim()) {
-        return decodeURIComponent(cookiePair[1]);
-      }
-    }
-    return null;
-  }
+// export function getCookie(cookieName) {
+//     var cookieArr = document.cookie.split(";");
+//     for (var i = 0; i < cookieArr.length; i++) {
+//       var cookiePair = cookieArr[i].split("=");
+//       if (cookieName === cookiePair[0].trim()) {
+//         return decodeURIComponent(cookiePair[1]);
+//       }
+//     }
+//     return null;
+//   }
+
+import Cookies from "js-cookie";
+export function getCookie(cookieName){
+  return Cookies.get(cookieName);
+}
 
 
